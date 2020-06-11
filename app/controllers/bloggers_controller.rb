7c21facd5d2c1689645c8f8257d5dd2afcc828ee
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-06-10 17:10:28 -0700

    Done.

---
+++ app/controllers/bloggers_controller.rb
@@ -0,0 +1,26 @@
+class BloggersController < ApplicationController 
+    def index
+        @bloggers = Blogger.all
+    end
+    def new
+        @blogger = Blogger.new
+    end
+
+    def create
+        @blogger = Blogger.create(blogger_params)
+        redirect_to blogger_path(@blogger)
+    end
+
+    def show
+        @blogger = Blogger.find(params[:id])
+        
+    end
+    def update
+        @blogger = Blogger.find(params[:id])
+        redirect_to blogger_path(@blogger)
+    end
+    private
+    def blogger_params
+        params.require(:blogger).permit(:name, :bio, :age)
+    end
+end 
--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,7 @@
 class Blogger < ApplicationRecord
-
+    has_many :posts
+    has_many :destinations, through: :posts
+    validates :name, presence: true, uniqueness: true
+    validates :bio, presence: true, length: { minimum: 1}
+    validates :age, presence: true, length: { minimum: 1}
 end
